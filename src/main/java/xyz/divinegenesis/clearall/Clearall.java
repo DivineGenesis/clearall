9e5686a729dab1a024bdd649a6b96a37c58e2e62
Jesse McKee <DrZoddiak@gmail.com>
2019-04-17 20:48:59 -0500

    Compatability
    Made it a bit more compatible for future mod additions

--- src/main/java/xyz/divinegenesis/clearall/Clearall.java
+++ src/main/java/xyz/divinegenesis/clearall/Clearall.java
@@ -4,6 +4,7 @@ import baubles.api.BaublesApi;
 import io.github.nucleuspowered.nucleus.api.events.NucleusClearInventoryEvent;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.filter.cause.First;
@@ -18,12 +19,18 @@ public class Clearall {
     @Listener
     public void onClearAll (NucleusClearInventoryEvent.Post event,@First Player player) {
         if(event.isClearingAll()) {
-            int number = BaublesApi.getBaublesHandler(getNativePlayer(player)).getSlots();
-            for (int i = 0;i < number;i++) {
-                ItemStack stack = BaublesApi.getBaublesHandler(getNativePlayer(player)).getStackInSlot(i);
-                if (!stack.isEmpty()) {
-                    stack.setCount(-1);
-                }
+            if(Sponge.getPluginManager().isLoaded("baubles")) {
+                clearBaubles(player);
+            }
+        }
+    }
+
+    private static void clearBaubles (Player player) {
+        int number = BaublesApi.getBaublesHandler(getNativePlayer(player)).getSlots();
+        for (int i = 0;i < number;i++) {
+            ItemStack stack = BaublesApi.getBaublesHandler(getNativePlayer(player)).getStackInSlot(i);
+            if (!stack.isEmpty()) {
+                stack.setCount(-1);
             }
         }
     }
