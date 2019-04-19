9168be3ac20d8eac4e36c16fef9be6b01465621b
Jesse McKee <DrZoddiak@gmail.com>
2019-04-18 20:12:22 -0500

    Add Cylic support

--- src/main/java/xyz/divinegenesis/clearall/Clearall.java
+++ src/main/java/xyz/divinegenesis/clearall/Clearall.java
@@ -1,6 +1,8 @@
 package xyz.divinegenesis.clearall;
 
 import baubles.api.BaublesApi;
+import com.lothrazar.cyclicmagic.playerupgrade.storage.InventoryPlayerExtended;
+import com.lothrazar.cyclicmagic.util.UtilPlayerInventoryFilestorage;
 import io.github.nucleuspowered.nucleus.api.events.NucleusClearInventoryEvent;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -11,6 +13,8 @@ import org.spongepowered.api.event.filter.cause.First;
 import org.spongepowered.api.plugin.Dependency;
 import org.spongepowered.api.plugin.Plugin;
 
+
+
 @Plugin (id = "clearall", name = "Clearall",dependencies = {
         @Dependency(id = "nucleus")
 })
@@ -18,16 +22,21 @@ public class Clearall {
 
     @Listener
     public void onClearAll (NucleusClearInventoryEvent.Post event,@First Player player) {
-        if(event.isClearingAll()) {
-            if(Sponge.getPluginManager().isLoaded("baubles")) {
+
+        if (event.isClearingAll()) {
+            if (Sponge.getPluginManager().isLoaded("baubles")) {
                 clearBaubles(player);
+            }
+            if (Sponge.getPluginManager().isLoaded("cyclicmagic")) {
+                clearCyclic(player);
             }
         }
     }
 
     private static void clearBaubles (Player player) {
-        int number = BaublesApi.getBaublesHandler(getNativePlayer(player)).getSlots();
-        for (int i = 0;i < number;i++) {
+
+        int size = BaublesApi.getBaublesHandler(getNativePlayer(player)).getSlots();
+        for (int i = 0;i < size;i++) {
             ItemStack stack = BaublesApi.getBaublesHandler(getNativePlayer(player)).getStackInSlot(i);
             if (!stack.isEmpty()) {
                 stack.setCount(-1);
@@ -35,7 +44,20 @@ public class Clearall {
         }
     }
 
-    private static EntityPlayer getNativePlayer(Player player) {
+    private static void clearCyclic (Player player) {
+
+        InventoryPlayerExtended inventory = UtilPlayerInventoryFilestorage.getPlayerInventory(getNativePlayer(player));
+        int size = inventory.getSizeInventory();
+        for (int i = 0;i < size;i++) {
+            ItemStack stack = inventory.getStackInSlot(i);
+            if (!stack.isEmpty()) {
+                stack.setCount(-1);
+            }
+        }
+    }
+
+    private static EntityPlayer getNativePlayer (Player player) {
+
         return (EntityPlayer) player;
     }
 }
